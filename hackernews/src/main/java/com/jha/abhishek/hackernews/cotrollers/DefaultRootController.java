86d7ebaf30fd70dce204cdf6c192a0598bda740e
abhi-jha <abhi_jha1995@rediffmail.com>
2018-03-10 03:01:30 +0530

    A non-documented deafult handler/controller

--- hackernews/src/main/java/com/jha/abhishek/hackernews/cotrollers/DefaultRootController.java
+++ hackernews/src/main/java/com/jha/abhishek/hackernews/cotrollers/DefaultRootController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import springfox.documentation.annotations.ApiIgnore;
+
 @Controller
+@ApiIgnore
 public class DefaultRootController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String defaultResponse() {
