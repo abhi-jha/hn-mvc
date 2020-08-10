c117bc626280c56126913117ca1a2fee5fe6f894
Abhishek Jha <abhi_jha1995@rediffmail.com>
2020-08-11 02:01:31 +0530

    Testing connection pooling

--- hackernews/src/main/java/com/jha/abhishek/hackernews/HackernewsApplication.java
+++ hackernews/src/main/java/com/jha/abhishek/hackernews/HackernewsApplication.java
@@ -1,12 +1,20 @@
 package com.jha.abhishek.hackernews;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class HackernewsApplication {
+	@Autowired
+	DataSource dataSource;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HackernewsApplication.class, args);
+//		SpringApplication.run(HackernewsApplication.class, args);
+//		SpringApplication.run(HackernewsApplication.class, args);
 	}
 }
